45e7b4a83f993e385327a56726b6a460e305681d
JieLiu-5 <33255781+JieLiu-5@users.noreply.github.com>
2021-06-30 11:28:06 +0800

    Add files via upload

---
+++ 13 cmake/c++ make/build/CMakeFiles/Test_for_Cmake.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Test_for_Cmake.dir/src/main.cpp.o"
+  "Test_for_Cmake.pdb"
+  "Test_for_Cmake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Test_for_Cmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
